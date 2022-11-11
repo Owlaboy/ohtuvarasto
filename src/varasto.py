5ce51a59852ee3fc0461dabe30aeccf885a5822b
Owlaboy <andrei.preotesa.ap@gmail.com>
2022-11-11 16:26:40 +0200

    added pylint checking

--- src/varasto.py
+++ src/varasto.py
@@ -1,22 +1,14 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        self.tilavuus = max(0, tilavuus)
 
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
-            # mahtuu
+        if alku_saldo > 0.0:
+            self.saldo = 0
+        if alku_saldo > self.tilavuus:
+            self.saldo = self.tilavuus
+        else:
             self.saldo = alku_saldo
-        else:
-            # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
